57cca3f85316d4a210794eb91a6f8636b0c7f736
Parami <paramiashinsana@gmail.com>
2024-07-31 13:17:50 +0530

    update

--- app/Analytics.tsx
+++ app/Analytics.tsx
@@ -103,6 +103,11 @@
 // });
 
 
+
+
+
+
+
 import React from 'react';
 import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
@@ -112,44 +117,16 @@ const { width } = Dimensions.get('window');
 export default function Analytics() {
     return (
         <View style={styles.container}>
-            {/* Original */}
-            {/* <FontAwesome name="arrow-left" size={24} color="grey" style={styles.backIcon} /> */}
+            <FontAwesome name="arrow-left" size={24} color="grey" style={styles.backIcon} />
             <View style={styles.card}>
-
                 <View style={styles.textContainer}>
-                    <FontAwesome name="arrow-left" size={24} color="grey" style={styles.backIcon} />
-                    <Text style={styles.cardText1}>Analytics & tools</Text>
+                    <Text style={styles.cardText1}>
+                        Analytics & tools
+                    </Text>
                     <Text style={styles.cardText2}>Last 30 days</Text>
                 </View>
                 <Image source={require('../assets/images/my-profile-image.jpg')} style={styles.profileImage} />
             </View>
-
-
-
-
-            {/* <View style={styles.secondCard}>
-            <Text style={styles.cardText3}>Analytics & tools</Text>
-            <FontAwesome name="questionmark" size={24} color="grey"/>
-                <View style={styles.quadCard}>
-                    <View style={styles.quadRow}>
-                        <View style={styles.quadBox}>
-                            <Text style={styles.quadText}>Text 1</Text>
-                        </View>
-                        <View style={styles.quadBox}>
-                            <Text style={styles.quadText}>Text 2</Text>
-                        </View>
-                    </View>
-                    <View style={styles.quadRow}>
-                        <View style={styles.quadBox}>
-                            <Text style={styles.quadText}>Text 3</Text>
-                        </View>
-                        <View style={styles.quadBox}>
-                            <Text style={styles.quadText}>Text 4</Text>
-                        </View>
-                    </View>
-                </View>
-            </View> */}
-
 
             <View style={styles.secondCard}>
                 <View style={styles.secondCardHeader}>
@@ -184,43 +161,31 @@ export default function Analytics() {
                 </View>
             </View>
 
-
-
             <View style={styles.thirdCard}>
                 <View style={styles.secondCardHeader}>
                     <Text style={styles.thirdTopic}>Creation tools</Text>
                 </View>
 
                 <View style={styles.firstLine}>
-                    <Text style={styles.paragraphText}>Get more ways to start conversaions with your community.Learn more
-                        about creation tool access.
-                    </Text>
+                    <Text style={styles.paragraphText}>Get more ways to start conversations with your community. Learn more about creation tool access.</Text>
                 </View>
 
                 <View style={styles.secondLine}>
+                    <View style={styles.secondRow}>
+                        <Text style={styles.paragraphText}>LinkedIn Live</Text>
+                        <FontAwesome name="times-circle-o" size={24} color="grey" style={styles.icon} />
+                        <Text style={styles.paragraphText1}>Learn more</Text>
+                        <FontAwesome name="arrow-right" size={24} color="grey" style={styles.icon} />
+                    </View>
 
                     <View style={styles.secondRow}>
-                        <Text style={styles.paragraphText}>LInkedIn Live</Text>
-                        {/* <View>
-                        <FontAwesome name="times-circle-o" size={24} color="grey"/>
-                        <Text style={styles.paragraphText}>LInkedIn Live</Text>
-                        </View> */}
-                    </View>
-
-
-                    <View >
                         <Text style={styles.paragraphText}>Audio Event</Text>
+                        <FontAwesome name="times-circle-o" size={24} color="grey" style={styles.icon} />
+                        <Text style={styles.paragraphText1}>Learn more</Text>
+                        <FontAwesome name="arrow-right" size={24} color="grey" style={styles.icon} />
                     </View>
-
-
                 </View>
-
             </View>
-
-
-
-
-
         </View>
     );
 }
@@ -228,17 +193,12 @@ export default function Analytics() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        // padding: 20,
         backgroundColor: '#f0f0f0',
         paddingTop: 40,
     },
     backIcon: {
-        marginBottom: 40, // Add some space below the icon
-        marginLeft: 10,
-    },
-    questionIcon: {
-        marginRight: 200,
-        marginBottom: 20,
+        marginBottom: 10,
+        marginLeft: 20,
     },
     card: {
         flexDirection: 'row',
@@ -251,18 +211,21 @@ const styles = StyleSheet.create({
         shadowRadius: 10,
         elevation: 5,
         marginBottom: 20,
+        marginHorizontal: 20,
     },
     textContainer: {
         flex: 1,
+        // flexDirection: 'row',
+        // alignItems: 'flex-start',
     },
-    //   cardText: {
-    //     fontSize: 16,
-    //     marginBottom: 5,
-    //   },
     cardText1: {
         fontSize: 20,
         marginBottom: 5,
         fontWeight: 'bold',
+        marginRight: 10,
+    },
+    questionIcon: {
+        marginLeft: 5,
     },
     cardText2: {
         fontSize: 14,
@@ -276,23 +239,8 @@ const styles = StyleSheet.create({
     profileImage: {
         width: 60,
         height: 60,
-        borderRadius: 25,
-        marginTop: 60,
+        borderRadius: 30,
     },
-    quadCard: {
-        // backgroundColor: '#fff',
-        // borderRadius: 10,
-        // shadowColor: '#000',
-        // shadowOpacity: 0.1,
-        // shadowRadius: 10,
-        // elevation: 5,
-    },
-    // secondCard: {
-    //     padding: 30,
-    //     borderColor: '#ddd',
-    //     borderWidth: 0.5,
-    //     backgroundColor: '#ffffff',
-    // },
     secondCard: {
         backgroundColor: '#fff',
         borderRadius: 10,
@@ -300,7 +248,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowRadius: 10,
         elevation: 5,
-        // marginHorizontal: 20, // Center the card with equal margin on both sides
+        marginHorizontal: 20,
         padding: 20,
     },
     thirdCard: {
@@ -311,8 +259,8 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowRadius: 10,
         elevation: 5,
-        // marginHorizontal: 20, // Center the card with equal margin on both sides
         padding: 20,
+        marginHorizontal: 20,
     },
     secondCardHeader: {
         flexDirection: 'row',
@@ -320,12 +268,15 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginBottom: 10,
     },
+    quadCard: {
+        // Style definitions for the quadCard
+    },
     quadRow: {
         flexDirection: 'row',
     },
     quadBox: {
         flex: 1,
-        padding: 20,
+        padding: 17,
         borderColor: '#ddd',
         borderWidth: 0.7,
         justifyContent: 'center',
@@ -333,9 +284,6 @@ const styles = StyleSheet.create({
         margin: 10,
         borderRadius: 5,
     },
-    // quadText: {
-    //     fontSize: 16,
-    // },
     quadText1: {
         fontSize: 20,
         fontWeight: 'bold',
@@ -353,16 +301,298 @@ const styles = StyleSheet.create({
     },
     paragraphText: {
         fontSize: 14,
+        flex: 1,
+    },
+    paragraphText1: {
+        fontSize: 14,
+        marginLeft: 10,
     },
     firstLine: {
-        alignItems: 'center',
+        marginBottom: 10,
     },
     secondLine: {
+        marginTop: 10,
+    },
+    secondRow: {
+        flexDirection: 'row',
         alignItems: 'center',
+        marginBottom: 10,
     },
-    secondRow:{
-      flexDirection:'row',
+    icon: {
+        marginLeft: 10,
     },
-    
 });
+
+
+
+
+
+
+
+// import React from 'react';
+// import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+// import { FontAwesome } from '@expo/vector-icons';
+
+// const { width } = Dimensions.get('window');
+
+// export default function Analytics() {
+//     return (
+//         <View style={styles.container}>
+//             {/* Original */}
+//             {/* <FontAwesome name="arrow-left" size={24} color="grey" style={styles.backIcon} /> */}
+//             <View style={styles.card}>
+
+//                 <View style={styles.textContainer}>
+//                     <FontAwesome name="arrow-left" size={24} color="grey" style={styles.backIcon} />
+//                     <Text style={styles.cardText1}>Analytics & tools</Text>
+//                     <Text style={styles.cardText2}>Last 30 days</Text>
+//                 </View>
+//                 <Image source={require('../assets/images/my-profile-image.jpg')} style={styles.profileImage} />
+//             </View>
+
+
+
+
+//             {/* <View style={styles.secondCard}>
+//             <Text style={styles.cardText3}>Analytics & tools</Text>
+//             <FontAwesome name="questionmark" size={24} color="grey"/>
+//                 <View style={styles.quadCard}>
+//                     <View style={styles.quadRow}>
+//                         <View style={styles.quadBox}>
+//                             <Text style={styles.quadText}>Text 1</Text>
+//                         </View>
+//                         <View style={styles.quadBox}>
+//                             <Text style={styles.quadText}>Text 2</Text>
+//                         </View>
+//                     </View>
+//                     <View style={styles.quadRow}>
+//                         <View style={styles.quadBox}>
+//                             <Text style={styles.quadText}>Text 3</Text>
+//                         </View>
+//                         <View style={styles.quadBox}>
+//                             <Text style={styles.quadText}>Text 4</Text>
+//                         </View>
+//                     </View>
+//                 </View>
+//             </View> */}
+
+
+//             <View style={styles.secondCard}>
+//                 <View style={styles.secondCardHeader}>
+//                     <Text style={styles.cardText3}>Analytics & tools</Text>
+//                     <FontAwesome name="question-circle" size={24} color="grey" style={styles.questionIcon} />
+//                 </View>
+//                 <View style={styles.quadCard}>
+//                     <View style={styles.quadRow}>
+//                         <View style={styles.quadBox}>
+//                             <Text style={styles.quadText1}>1464</Text>
+//                             <Text style={styles.quadText2}>Post impressions</Text>
+//                             <Text style={styles.quadText3}>🔻58.9% past 7 days</Text>
+//                         </View>
+//                         <View style={styles.quadBox}>
+//                             <Text style={styles.quadText1}>3678</Text>
+//                             <Text style={styles.quadText2}>Followers</Text>
+//                             <Text style={styles.quadText3}>🔺51.6% past 7 days</Text>
+//                         </View>
+//                     </View>
+//                     <View style={styles.quadRow}>
+//                         <View style={styles.quadBox}>
+//                             <Text style={styles.quadText1}>297</Text>
+//                             <Text style={styles.quadText2}>Profile viewers</Text>
+//                             <Text style={styles.quadText3}>Past 90 days</Text>
+//                         </View>
+//                         <View style={styles.quadBox}>
+//                             <Text style={styles.quadText1}>364</Text>
+//                             <Text style={styles.quadText2}>Search appearances</Text>
+//                             <Text style={styles.quadText3}>Previous week</Text>
+//                         </View>
+//                     </View>
+//                 </View>
+//             </View>
+
+
+
+//             <View style={styles.thirdCard}>
+//                 <View style={styles.secondCardHeader}>
+//                     <Text style={styles.thirdTopic}>Creation tools</Text>
+//                 </View>
+
+//                 <View style={styles.firstLine}>
+//                     <Text style={styles.paragraphText}>Get more ways to start conversaions with your community.Learn more
+//                         about creation tool access.
+//                     </Text>
+//                 </View>
+
+//                 <View style={styles.secondLine}>
+
+//                     <View style={styles.secondRow}>
+//                         <Text style={styles.paragraphText}>LinkedIn Live</Text>
+//                         <View>
+//                         <FontAwesome name="times-circle-o" size={24} color="grey"/>
+//                         <Text style={styles.paragraphText1}>Learn more</Text>
+//                         </View>
+//                     </View>
+
+
+//                     <View >
+//                         <Text style={styles.paragraphText}>Audio Event</Text>
+//                     </View>
+
+
+//                 </View>
+
+//             </View>
+
+
+
+
+
+//         </View>
+//     );
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         // padding: 20,
+//         backgroundColor: '#f0f0f0',
+//         paddingTop: 40,
+//     },
+//     backIcon: {
+//         marginBottom: 40, // Add some space below the icon
+//         marginLeft: 10,
+//     },
+//     questionIcon: {
+//         marginRight: 200,
+//         marginBottom: 20,
+//     },
+//     card: {
+//         flexDirection: 'row',
+//         alignItems: 'center',
+//         backgroundColor: '#fff',
+//         padding: 20,
+//         borderRadius: 10,
+//         shadowColor: '#000',
+//         shadowOpacity: 0.1,
+//         shadowRadius: 10,
+//         elevation: 5,
+//         marginBottom: 20,
+//     },
+//     textContainer: {
+//         flex: 1,
+//     },
+//     //   cardText: {
+//     //     fontSize: 16,
+//     //     marginBottom: 5,
+//     //   },
+//     cardText1: {
+//         fontSize: 20,
+//         marginBottom: 5,
+//         fontWeight: 'bold',
+//     },
+//     cardText2: {
+//         fontSize: 14,
+//         marginBottom: 5,
+//     },
+//     cardText3: {
+//         fontSize: 20,
+//         marginBottom: 20,
+//         fontWeight: 'bold',
+//     },
+//     profileImage: {
+//         width: 60,
+//         height: 60,
+//         borderRadius: 25,
+//         marginTop: 60,
+//     },
+//     quadCard: {
+//         // backgroundColor: '#fff',
+//         // borderRadius: 10,
+//         // shadowColor: '#000',
+//         // shadowOpacity: 0.1,
+//         // shadowRadius: 10,
+//         // elevation: 5,
+//     },
+//     // secondCard: {
+//     //     padding: 30,
+//     //     borderColor: '#ddd',
+//     //     borderWidth: 0.5,
+//     //     backgroundColor: '#ffffff',
+//     // },
+//     secondCard: {
+//         backgroundColor: '#fff',
+//         borderRadius: 10,
+//         shadowColor: '#000',
+//         shadowOpacity: 0.1,
+//         shadowRadius: 10,
+//         elevation: 5,
+//         // marginHorizontal: 20, // Center the card with equal margin on both sides
+//         padding: 20,
+//     },
+//     thirdCard: {
+//         marginTop: 10,
+//         backgroundColor: '#fff',
+//         borderRadius: 10,
+//         shadowColor: '#000',
+//         shadowOpacity: 0.1,
+//         shadowRadius: 10,
+//         elevation: 5,
+//         // marginHorizontal: 20, // Center the card with equal margin on both sides
+//         padding: 20,
+//     },
+//     secondCardHeader: {
+//         flexDirection: 'row',
+//         justifyContent: 'space-between',
+//         alignItems: 'center',
+//         marginBottom: 10,
+//     },
+//     quadRow: {
+//         flexDirection: 'row',
+//     },
+//     quadBox: {
+//         flex: 1,
+//         padding: 20,
+//         borderColor: '#ddd',
+//         borderWidth: 0.7,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         margin: 10,
+//         borderRadius: 5,
+//     },
+//     // quadText: {
+//     //     fontSize: 16,
+//     // },
+//     quadText1: {
+//         fontSize: 20,
+//         fontWeight: 'bold',
+//     },
+//     quadText2: {
+//         fontSize: 14,
+//     },
+//     quadText3: {
+//         fontSize: 12,
+//         color: '#999999',
+//     },
+//     thirdTopic: {
+//         fontSize: 20,
+//         fontWeight: 'bold',
+//     },
+//     paragraphText: {
+//         fontSize: 14,
+//     },
+//     paragraphText1: {
+//         fontSize: 14,
+        
+//     },
+//     firstLine: {
+//         alignItems: 'center',
+//     },
+//     secondLine: {
+//         alignItems: 'center',
+//     },
+//     secondRow:{
+//       flexDirection:'row',
+//     },
+    
+// });
 
